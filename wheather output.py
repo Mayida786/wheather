3824acafce433c59a364930b46da0fa8a2ad5df7
Mayida786 <86304429+Mayida786@users.noreply.github.com>
2021-06-23 13:18:46 +0530

    Uploaded the project file

---
+++ wheather output.py
@@ -0,0 +1,8 @@
+Enter the city name: mumbai
+-------------------------------------------------------------
+Weather Stats for - MUMBAI  || 22 Jun 2021 | 06:44:28 PM
+-------------------------------------------------------------
+Current temperature is: 30.99 deg C
+Current weather desc  : haze
+Current Humidity      : 74 %
+Current wind speed    : 6.69 kmph
